26ca1bdd4e3e69e04b3f818ff9583c5bcee4f567
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-06-05 22:58:41 +0900

    up

---
+++ memo.md
@@ -0,0 +1,12 @@
+# 📝 2022/06/05
+
+A4(440.0Hz) を気軽に吐きたい
+
+[WAV - Wikipedia](https://ja.m.wikipedia.org/wiki/WAV)
+
+> データ長が32ビット符号なし整数型で記述されている
+
+`uint`
+
+
+[Pythonでのwavファイル操作 - Qiita](https://qiita.com/Dsuke-K/items/2ad4945a81644db1e9ff)
--- simpleSineWaveOut/binary_check.py
+++ simpleSineWaveOut/binary_check.py
@@ -12,15 +12,31 @@ wv = [
   for t in range(sampling_rate * write_sec)
 ]
 # バイナリ化の下準備の下準備
-max_num = 32767.0 / max(wv)
+max_num = 32767.0 / max(wv)  # xxx: short ?
 # バイナリ化の下準備
 wv16 = [int(x * max_num) for x in wv]
+print(len(wv16))
 # バイナリ化
 bi_wv = struct.pack('h' * len(wv16), *wv16)
+print(len(bi_wv))
+bibi = struct.pack('f' * len(wv), *wv)
+#bibi = b''.join([struct.pack('I', v) for v in wv])
+
+#by_wv = bytearray.fromhex(wv16)
 
 with wave.open(out_put, mode='wb') as f:
   # todo: (nchannels, sampwidth, framerate, nframes, comptype, compname)
-  param = (1, 2, sampling_rate, len(bi_wv), 'NONE', 'not compressed')  #パラメータ
+  param = (2, 2, sampling_rate, len(bi_wv), 'NONE', 'not compressed')  #パラメータ
   f.setparams(param)  #パラメータの設定
-  f.writeframes(bi_wv)  #データの書き込み
+  #f.writeframes(bi_wv)  #データの書き込み
+  f.writeframes(bibi)  #データの書き込み
+
+  nchannles = f.getnchannels()
+  samplewidth = f.getsampwidth()
+  framerate = f.getframerate()
+  nframes = f.getnframes()
+  print(f'Channel num : {nchannles}')
+  print(f'Sample width : {samplewidth}')
+  print(f'Sampling rate : {framerate}')
+  print(f'Frame num : {nframes}')
 
