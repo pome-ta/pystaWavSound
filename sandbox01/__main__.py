9f7f97fe490c380edc84d94f7a4384171a3ad9f6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-06-09 00:18:33 +0900

    out!

--- sandbox01/__main__.py
+++ sandbox01/__main__.py
@@ -1,24 +1,28 @@
 from pathlib import Path
-import array
 import wave
 
+import struct
+#import array
+
 wav_data_path = Path('./dump/wavData.txt')
+out_put = './out/out.wav'
 #wav_txt = wav_data_path.read_text()
 
-DURATION = 0.1
+DURATION = 16
 sample = 44100
 
 with open(wav_data_path, encoding='utf_8') as f:
   l_strip = [float(s.strip()) for s in f.readlines()]
   #print(l_strip)
 
-buf = array.array('f', l_strip)
-out_put = './out/out.wav'
+max_num = 32767.0 / max(l_strip)
+wv16 = [int(i * max_num) for i in l_strip]
+buf = struct.pack('h' * len(wv16), *wv16)
+#buf = array.array('f', l_strip)
 
 with wave.open(out_put, mode='wb') as f:
   # # todo: (nchannels, sampwidth, framerate, nframes, comptype, compname)
-  param = (2, 4, sample, int(sample * DURATION), 'NONE', 'not compressed')
+  param = (2, 2, sample, sample * DURATION, 'NONE', 'not compressed')
   f.setparams(param)
   f.writeframes(buf)
-
 
