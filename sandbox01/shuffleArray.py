1d1ded5d4f24b3602f306531dcec2e69ce51fd75
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-06-09 12:08:24 +0900

    up

--- sandbox01/shuffleArray.py
+++ sandbox01/shuffleArray.py
@@ -3,9 +3,10 @@ import wave
 
 import struct
 #import array
+import random
 
 wav_data_path = Path('./dump/wavData.txt')
-out_put = './out/out.wav'
+out_put = './out/reverse.wav'
 #wav_txt = wav_data_path.read_text()
 
 DURATION = 16
@@ -14,6 +15,8 @@ sample = 44100
 with open(wav_data_path, encoding='utf_8') as f:
   l_strip = [float(s.strip()) for s in f.readlines()]
 
+#random.shuffle(l_strip)
+l_strip.reverse()
 max_num = 32767.0 / max(l_strip)
 wv16 = [int(i * max_num) for i in l_strip]
 buf = struct.pack('h' * len(wv16), *wv16)
